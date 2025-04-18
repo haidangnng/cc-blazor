8f08ee94f5daae094767d4452611046826cc07b6
haidangnng <haidangnng@gmail.com>
2025-04-18 10:45:34 +0200

    update: rename

--- Quizlet.Client/Models/Course.cs
+++ Quizlet.Client/Models/Course.cs
@@ -1,9 +1,7 @@
 namespace Quizlet.Client.Models;
-public class Room
+public class Course
 {
     public string Id { get; set; }
     public string Name { get; set; }
-    public string Password { get; set; }
-    public string Sources { get; set; }
 }
 
